87fc216ca3ef8419ccdefdefeca0f9b68e65d160
DESKTOP-GRLN2UF\JAIME <jaime.supo@yacutech.com>
2023-11-30 21:04:46 -0500

    Rutas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,39 +1,11 @@
 <?php
 
-namespace Config;
-
 use CodeIgniter\Router\RouteCollection;
 
-// Create a new instance of our RouteCollection class.
-$routes = Services::routes();
-
-/*
- * --------------------------------------------------------------------
- * Router Setup
- * --------------------------------------------------------------------
+/**
+ * @var RouteCollection $routes
  */
-$routes->setDefaultNamespace('App\Controllers');
-$routes->setDefaultController('Home');
-$routes->setDefaultMethod('index');
-$routes->setTranslateURIDashes(false);
-$routes->set404Override();
-// The Auto Routing (Legacy) is very dangerous. It is easy to create vulnerable apps
-// where controller filters or CSRF protection are bypassed.
-// If you don't want to define all routes, please use the Auto Routing (Improved).
-// Set `$autoRoutesImproved` to true in `app/Config/Feature.php` and set the following to true.
-// $routes->setAutoRoute(false);
-
-/*
- * --------------------------------------------------------------------
- * Route Definitions
- * --------------------------------------------------------------------
- */
-
-// We get a performance increase by specifying the default
-// route since we don't have to scan directories.
-
 $routes->get('/', 'Home::index');
-
 $routes->get('/Guia', 'Guia::index');
 
 $routes->get('/Liquidacion', 'Liquidacion::index');
@@ -42,5 +14,15 @@ $routes->get('/Recepcion/obtenerDatos', 'Guia::obtenerTodosLosDatos');
 $routes->post('/Recepcion/eliminar/(:any)', 'Guia::eliminar/$1');
 $routes->get('/Recepcion/transferir', 'Guia::transferirDatos');
 
+// rutas  braulio
+$routes->get('/Usuarios', 'Usuarios::usuario');
+$routes->get('/Productos', 'Productos::producto');
+$routes->get('/Configuracion', 'Configuraciones::configuracion');
+$routes->get('/Servicio', 'Servicios::servicio');
+
+$routes->post('/Productos/registrar', 'Productos::registrar');
+$routes->post('/Configuraciones/registrar', 'Configuraciones::registrar');
+$routes->post('/Servicios/registrar', 'Servicios::registrar');
+$routes->post('/Usuarios/registrar', 'Usuarios::registrar');
 
 
